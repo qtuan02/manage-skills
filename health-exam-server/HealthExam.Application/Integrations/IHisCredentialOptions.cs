e6df17e1257d0d9e4521ab8c45e4e64c3278f516
HC-TuanHQ5 <hc-tuanhq5@skills-manager.local>
2026-09-22 10:55:00 +0700

    auto backup
    Skills-Manager-Protocol: 2

--- health-exam-server/HealthExam.Application/Integrations/IHisCredentialOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace HealthExam.Application.Integrations;
-
-public interface IHisCredentialOptions
-{
-    string CredentialHeaderName { get; }
-    int? KskDepartmentId => null;
-    string KskDepartmentCode => null;
-}
--- health-exam-server/HealthExam.Application/Paraclinical/IOrderNoAllocator.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace HealthExam.Application.Paraclinical;
-
-public interface IOrderNoAllocator
-{
-    Task<string> NextAsync(CancellationToken ct = default);
-}
